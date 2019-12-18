b6719b89968d91325e6793d23dd286f651ea1e0b
Thiago Viotto <thiagohenriqueviotto1@gmail.com>
2019-12-18 17:13:07 -0300

    fix #48

--- my-series/backend/src/server.js
+++ my-series/backend/src/server.js
@@ -7,7 +7,7 @@ app.use(cors())
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'public/images')
+    cb(null, '../frontend/public/images')
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname)
