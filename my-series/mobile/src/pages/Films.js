eab9e66af7f66b302d67d37f2b8b1a3fcf18a549
Thiago Viotto <thiagohenriqueviotto1@gmail.com>
2020-04-13 21:59:26 -0300

    fix #105

--- my-series/mobile/src/pages/Films.js
+++ my-series/mobile/src/pages/Films.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Button, Title } from 'native-base';
 import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native'
 import api from '../services/api'
+import { ScrollView } from 'react-native-gesture-handler';
 
 const statuses = {
     "watched": "Assistido",
@@ -11,7 +12,6 @@ const statuses = {
 
 function Films({ route, navigation }) {
     const item = route.params.item.genre
-    console.log(item)
     const [films, setFilms] = useState([])
 
     useEffect(() => {
@@ -24,21 +24,42 @@ function Films({ route, navigation }) {
         loadFilms()
     }, [])
 
+    handleFavorite = (item) => {
+        addFavorite(item)
+    }
+
+    async function addFavorite(item) {
+        const editFilm = {
+            id: item.id,
+            name: item.name,
+            comment: item.comment,
+            status: item.status,
+            genre: 'favorite',
+            img: item.img,
+            nameImage: item.nameImage,
+            video: item.video
+        }
+        await api.put('films/' + editFilm.id, editFilm)
+        await navigation.navigate('Favorite', { params: { item } })
+    }
+
     return (
         <>
             {films.map(film => (
-                <View style={styles.content}>
-                    < TouchableOpacity key={film.id} style={styles.imgGenreView} >
-                        <Image source={{ uri: `http://10.0.3.2:3000/images/${film.nameImage}` }} style={styles.imgGenre} />
-                        <Title style={styles.name}>{film.name}</Title>
-                        <Text style={styles.status}>{film.genre} / {statuses[film.status]}</Text>
-                        <View style={styles.containerButton}>
-                            <Button rounded info style={styles.button}><Text style={{ paddingHorizontal: 17, fontWeight: 'bold', color: '#FFFFFF', textShadowRadius: 5, fontSize: 15, textShadowColor: '#000000'}}>Favoritos</Text></Button>
-                            <Button rounded bordered style={styles.button}><Text style={{ paddingHorizontal: 28, fontWeight: 'bold', color: '#007bff', fontSize: 15}}>Editar</Text></Button>
-                            <Button rounded danger style={styles.button}><Text style={{ paddingHorizontal: 27, fontWeight: 'bold', color: '#FFFFFF', fontSize: 15, textShadowRadius: 5, textShadowColor: '#000000'}}>Excluir</Text></Button>
-                        </View>
-                    </TouchableOpacity>
-                </View>
+                <ScrollView>
+                    <View style={styles.content}>
+                        <TouchableOpacity key={film.id} style={styles.imgGenreView} >
+                            <Image source={{ uri: `http://10.0.3.2:3000/images/${film.nameImage}` }} style={styles.imgGenre} />
+                            <Title style={styles.name}>{film.name}</Title>
+                            <Text style={styles.status}>{film.genre} / {statuses[film.status]}</Text>
+                            <View style={styles.containerButton}>
+                                <Button rounded info style={styles.button} onPress={() => this.handleFavorite(film)}><Text style={{ paddingHorizontal: 17, fontWeight: 'bold', color: '#FFFFFF', textShadowRadius: 5, fontSize: 15, textShadowColor: '#000000' }}>Favoritos</Text></Button>
+                                <Button rounded bordered style={styles.button}><Text style={{ paddingHorizontal: 28, fontWeight: 'bold', color: '#007bff', fontSize: 15 }}>Editar</Text></Button>
+                                <Button rounded danger style={styles.button}><Text style={{ paddingHorizontal: 27, fontWeight: 'bold', color: '#FFFFFF', fontSize: 15, textShadowRadius: 5, textShadowColor: '#000000' }}>Excluir</Text></Button>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                </ScrollView>
             ))
             }
         </>
@@ -51,7 +72,8 @@ const styles = StyleSheet.create({
         borderRadius: 15,
         backgroundColor: '#FFFFFF',
         marginTop: 3,
-        paddingBottom: 7
+        paddingBottom: 7,
+        marginTop: 5
     },
     imgGenre: {
         width: '100%',
